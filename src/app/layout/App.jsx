a65d8ab7302a6b58d1b83a39ad756386c6be1105
Anderson Mattjie da SIlva <andersonmattjiedasilva@Andersons-MacBook-Pro.local>
2018-05-16 18:08:29 -0300

    Initial Commit

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <h1>Re-vents</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
